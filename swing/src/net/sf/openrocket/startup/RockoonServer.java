afaaba4911000b794b66b653e8bc539adc384a3b
Kai Marshland <kaimarshland@gmail.com>
2019-05-01 23:11:41 -0700

    Added basic dockerization

--- swing/src/net/sf/openrocket/startup/RockoonServer.java
+++ swing/src/net/sf/openrocket/startup/RockoonServer.java
@@ -45,6 +45,11 @@ public class RockoonServer {
 
     static private Map<String, String> queryToMap(String query) {
         Map<String, String> result = new HashMap<>();
+
+        if (query == null) {
+            return result;
+        }
+
         for (String param : query.split("&")) {
             String[] entry = param.split("=");
             if (entry.length > 1) {
